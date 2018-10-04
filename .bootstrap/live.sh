1ce230393ef7865a8e3ea198a490f0a8b5cbe259
Vuk Mirovic <wooque@gmail.com>
2018-10-04 05:14:21 +0200

    Fix

--- .bootstrap/live.sh
+++ .bootstrap/live.sh
@@ -3,7 +3,7 @@
 timedatectl set-ntp true
 mkfs.ext4 /dev/sda3
 mount /dev/sda3 /mnt
-pacstrap /mnt base base base-devel
+pacstrap /mnt base base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 sed -i 's/rw,relatime/rw,noatime/' /mnt/etc/fstab
 cp -r configs /mnt/opt/configs
