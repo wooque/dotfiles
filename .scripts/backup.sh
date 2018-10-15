4d906ba050e7305f4c41814dd87ed82c4a35c2b1
Vuk Mirovic <wooque@gmail.com>
2018-10-15 22:45:47 +0200

    Update backup script

--- .scripts/backup.sh
+++ .scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -azP -e "ssh -p 21299" --exclude='*.iso' --exclude='*.vdi' --exclude='*.vmdk' /mnt/PODACI root@51.15.75.156:/root/backup --delete 
+rsync -azP --delete -e "ssh -p 21299" --exclude-from='/home/vuk/.scripts/backup_exclude' /mnt/PODACI root@51.15.75.156:/root/backup
