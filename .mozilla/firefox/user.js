83f647097d2965bdb59e6619a30505304da5353c
Vuk Mirovic <wooque@gmail.com>
2019-01-12 23:20:18 +0100

    Add basic Firefox user.js

---
+++ .mozilla/firefox/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.fullscreen.autohide", false);
+user_pref("font.name-list.sans-serif.x-cyrillic", "Liberation Sans,Noto Color Emoji");
+user_pref("font.name-list.sans-serif.x-unicode", "Liberation Sans,Noto Color Emoji");
+user_pref("font.name-list.sans-serif.x-western", "Liberation Sans,Noto Color Emoji");
+user_pref("layers.acceleration.force-enabled", true);
