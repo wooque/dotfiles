7f1bd7297b535a72890508ff4caa98bb10e07975
Vuk Mirovic <wooque@gmail.com>
2019-01-13 01:02:56 +0100

    Update Firefox user.js

--- .mozilla/firefox/user.js
+++ .mozilla/firefox/user.js
@@ -2,4 +2,7 @@ user_pref("browser.fullscreen.autohide", false);
 user_pref("font.name-list.sans-serif.x-cyrillic", "Liberation Sans,Noto Color Emoji");
 user_pref("font.name-list.sans-serif.x-unicode", "Liberation Sans,Noto Color Emoji");
 user_pref("font.name-list.sans-serif.x-western", "Liberation Sans,Noto Color Emoji");
+user_pref("font.name-list.serif.x-cyrillic", "Liberation Serif,Noto Color Emoji");
+user_pref("font.name-list.serif.x-unicode", "Liberation Serif,Noto Color Emoji");
+user_pref("font.name-list.serif.x-western", "Liberation Serif,Noto Color Emoji");
 user_pref("layers.acceleration.force-enabled", true);
