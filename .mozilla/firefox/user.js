cbe5d5894083d096d77814c593235a2345cdfa88
Vuk Mirovic <contact@vukmirovic.org>
2019-03-27 00:54:34 +0100

    Use Droid font instead of DejaVu. Remove Firefox's user.js.

--- .mozilla/firefox/user.js
+++
@@ -1,7 +0,0 @@
-user_pref("font.name-list.sans-serif.x-cyrillic", "Liberation Sans,Noto Color Emoji");
-user_pref("font.name-list.sans-serif.x-unicode", "Liberation Sans,Noto Color Emoji");
-user_pref("font.name-list.sans-serif.x-western", "Liberation Sans,Noto Color Emoji");
-user_pref("font.name-list.serif.x-cyrillic", "Liberation Serif,Noto Color Emoji");
-user_pref("font.name-list.serif.x-unicode", "Liberation Serif,Noto Color Emoji");
-user_pref("font.name-list.serif.x-western", "Liberation Serif,Noto Color Emoji");
-user_pref("layers.acceleration.force-enabled", true);
