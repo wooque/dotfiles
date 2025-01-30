f9afc1ca1193e147a5d288617e7fba77aacd29cf
Vuk Mirovic <contact@vukmirovic.org>
2025-01-30 16:51:49 +0100

    mpv restart on finish

---
+++ .config/mpv/scripts/on_finish.lua
@@ -0,0 +1,5 @@
+mp.observe_property("pause", "bool", function(name, value)
+  if value == true and mp.get_property("eof-reached") == "yes" then
+    mp.command("no-osd seek 0 absolute")
+  end
+end)
