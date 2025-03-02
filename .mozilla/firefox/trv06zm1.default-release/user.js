d544054df9e6586e7695a5799311e00f4eb70a5b
Vuk Mirovic <contact@vukmirovic.org>
2025-03-02 19:58:07 +0100

    firefox, disable app.normandy.api_url as well

--- .mozilla/firefox/trv06zm1.default-release/user.js
+++ .mozilla/firefox/trv06zm1.default-release/user.js
@@ -1,6 +1,7 @@
 user_pref("apz.gtk.kinetic_scroll.enabled", false);
 user_pref("apz.overscroll.enabled", false);
 user_pref("app.normandy.enable", false);
+user_pref("app.normandy.api_url", "");
 user_pref("app.shield.optoutstudies.enabled", false);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.contentblocking.category", "custom");
